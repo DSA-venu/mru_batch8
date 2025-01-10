4bc7f913ca81c35764a51c3edcec941fbcad7e95
DSA-venu <pindivenukumarreddy@gmail.com>
2025-01-10 14:50:29 +0530

    oops completed

---
+++ src/com/mru/oops/Interface1.java
@@ -0,0 +1,16 @@
+package com.mru.oops;
+
+
+
+	interface Interface1 {
+ 
+		public void addition();
+		
+		public int subtraction(int num1,int num2);
+		
+		public int multiplication(int num1,int num2);
+		public int Division();
+	}
+
+
+
